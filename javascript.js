af236533ca436fe69010bc4fe913187da6cf6f41
Preslav977 <p.cvetanow@gmail.com>
2023-01-04 10:40:23 +0200

    Completely rewrote the project with added functionality

--- javascript.js
+++ javascript.js
@@ -1,33 +1,136 @@
+//variables
 const gridContainer = document.getElementById('grid-container');
+const createBtnSmall = document.getElementById('create-button-small');
+const createBtnMedium = document.getElementById('create-button-medium');
+const createBtnBig = document.getElementById('create-button-big');
+const clearBtn = document.getElementById('clear-button');
+const changeColorBlue = document.getElementById('color-blue');
+const changeColorRed = document.getElementById('color-red');
+const changeColorGreen = document.getElementById('color-green');
+const Eraser = document.getElementById('button-eraser');
 
-function smallGrid () {
-    for (let i = 0; i < 144; i++) {
+//default grid creation
+function defaultGrid () {
+  for (let j = 0; j < 265; j++) {
       const square = document.createElement('div');
       square.classList.add('grid-content');
       gridContainer.appendChild(square);
-   }
-}
+    }
+  }
 
-smallGrid ()
+   defaultGrid(265);
+   
+   
+//small grid creation 
+createBtnSmall.addEventListener('click', function() {
+  deleteGrid ();
+  creatingGrid ();
 
-//hover event
-const allDivs = document.querySelectorAll('.grid-content');
+  const allDivs = document.querySelectorAll('.grid-content');
 allDivs.forEach((div) => {
   div.addEventListener('mouseover', (event) => {
-    div.style.backgroundColor = 'red';
+    div.style.backgroundColor = 'white';
   });
 })
+});
 
-function deleteGrid () {
+//medium grid creation
+createBtnMedium.addEventListener('click', function() {
+  deleteGrid ();
+  creatingGrid ();
+  const allDivs = document.querySelectorAll('.grid-content');
+allDivs.forEach((div) => {
+  div.addEventListener('mouseover', (event) => {
+    div.style.backgroundColor = 'white';
+  });
+})
+});
+
+//big grid creation
+createBtnBig.addEventListener('click', function() {
+  deleteGrid ();
+  creatingGrid ();
+  const allDivs = document.querySelectorAll('.grid-content');
+allDivs.forEach((div) => {
+  div.addEventListener('mouseover', (event) => {
+    div.style.backgroundColor = 'white';
+  });
+})
+});
+
+//Clear grid event 
+clearBtn.addEventListener('click', function() {
+  clearGrid();
+});
+
+//clear grid function
+function clearGrid () {
   const grid = document.querySelectorAll('.grid-content');
   grid.forEach(grid => {
-    grid.remove();
+    if (grid.style.backgroundColor !== "white") {
+      grid.style.backgroundColor = "white";
+    }
   })
 }
 
-const smallSketch = document.getElementById('small-grid');
-smallSketch.addEventListener('click', function() {
-  smallGrid(144);
-});
+//this function takes user input and base on button creates small, medium, big grids
+function creatingGrid () {
+  let userInput = prompt("Pick grid");
+  if (userInput > 100) {
+    console.log('ERROR');
+  } for (let i = 0; i < userInput; i++) {
+      for (let j = 0; j < userInput; j++) {
+      const square = document.createElement('div');
+      square.classList.add('grid-content');
+      gridContainer.appendChild(square);
+    }
+  }
+   }
 
+   //delete grid
+function deleteGrid() {
+  const allDivs = document.querySelectorAll('.grid-content');
+  allDivs.forEach(div => {
+    div.remove();
+  });
+}
+  
+  //event listener for changing color to blue
+    changeColorBlue.addEventListener('click', function() {
+      const allDivs = document.querySelectorAll('.grid-content');
+      allDivs.forEach((div) => {
+        div.addEventListener('mouseover', (event) => {
+          div.style.backgroundColor = 'blue';
+        });
+      })
+    });
+  
+  //event listener for changing color to red
+    changeColorRed.addEventListener('click', function() {
+      const allDivs = document.querySelectorAll('.grid-content');
+      allDivs.forEach((div) => {
+        div.addEventListener('mouseover', (event) => {
+          div.style.backgroundColor = 'red';
+        });
+      })
+    });
+  
+  //event listener for changing color to green
+    changeColorGreen.addEventListener('click', function() {
+      const allDivs = document.querySelectorAll('.grid-content');
+      allDivs.forEach((div) => {
+        div.addEventListener('mouseover', (event) => {
+          div.style.backgroundColor = 'green';
+        });
+      })
+    });
 
+	//event listener for changing color to act as eraser
+    Eraser.addEventListener('click', function() {
+      const allDivs = document.querySelectorAll('.grid-content');
+      allDivs.forEach((div) => {
+        div.addEventListener('mouseover', (event) => {
+          div.style.backgroundColor = 'white';
+        });
+      })
+    });
